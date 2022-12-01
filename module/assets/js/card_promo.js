d8df47ed13dfc959b6cac120e07e0f8d4a5e1153
SAMUEL RODRIGUES DE MATOS <22102420@dsn127m1241959.cfp127aluno.local>
2022-12-01 11:24:49 -0300

    fazendo as promocoes e favoritos JS

---
+++ module/assets/js/card_promo.js
@@ -0,0 +1,100 @@
+class card extends HTMLElement{
+    constructor(){
+        super();
+        this.shadow = this.attachShadow({mode:'open'});
+        this.bgcolor = '#E7A200'
+        this.image = 'Sem Imagem'
+        this.nome = 'Nome do Produto'
+        this.descricao = ''
+        this.preco = ''
+        this.id = ''
+    }
+    static get observedAttributes(){
+        return ['bgcolor', 'image', 'nome','descricao', 'preco', 'id']
+    }
+
+    attributeChangedCallback(nameAttr, oldValue, newValue){
+        //this.[nameAttr] = newValue
+
+        if (nameAttr === 'bgcolor') {
+            this.bgcolor = newValue
+        } else if (nameAttr === 'image') {
+            this.image = newValue
+        } else if (nameAttr === 'nome') {
+            this.nome = newValue
+        } else if (nameAttr === 'descricao') {
+            this.descricao = newValue
+        } else if (nameAttr === 'preco') {
+            this.preco = newValue
+        } else if (nameAttr === 'id') {
+            this.id = newValue
+        }
+    }
+
+    connectedCallback(){
+        this.shadow.appendChild(this.component());
+        this.shadow.appendChild(this.styles());
+    }
+
+    styles(){
+        const style = document.createElement('style');
+        style.textContent = 
+        `
+            .card_promo{
+                display: flex;
+                justify-content: space-between;
+                flex-direction: column;
+                background-color: #E7A200;
+                height: 420px;
+                width: 410px;
+                border-radius: 20px;
+                padding: 25px;
+            }
+            .card_promo:hover .number_card{
+                color: #d03012;
+                transition: 0.5s;
+            }
+            .card_promo img{
+                height: 220px;
+                border-radius: 12px;
+            }
+            .txt_card{
+                color: white;
+                font-family: 'Roboto', sans-serif;
+                gap: 5px;
+                font-size: 20px;
+                margin-top: 5px;
+                /* background-color: #4C5A5D; */
+            }
+            .number_card{
+                display: flex;
+                justify-content: flex-end;
+                color: white;
+                font-family: 'Roboto', sans-serif;
+                font-size: 38px;
+                font-weight: 800;
+                /* background-color: #d03012; */
+            }
+        `
+        return style;
+    }
+
+    component(){
+        const card = document.createElement('div');
+        card.classList.add('card_promo');
+        card.innerHTML = 
+        `
+        <img src="${this.image}" alt="">
+        <div class="txt_card">
+            <h1 class="name">${this.nome}</h1>
+            <span class="description">${this.descricao}</span>
+        </div>
+        <div class="number_card">
+            <span class="price">${this.preco}</span>
+        </div>
+
+        `
+        return card;
+    }
+}
+customElements.define('card_promo', card)
---
+++ module/assets/js/sectionFav.js
@@ -0,0 +1,47 @@
+'use strict'
+
+import './card_favoritos.js'
+
+const fetchPizza = async function () {
+    const url = ``
+    const response =  await fetch(url);
+    const data = await response.json();
+    return data
+}
+
+const pizza = await fetchPizza();
+console.log(pizza);
+
+pizza.forEach(element => {
+    const container = document.querySelector('.promocoes')
+    const card = document.createElement('.card_promo')
+
+    card.setAttribute('imagem', element.imagem)
+    card.setAttribute('nome', element.nome)
+    card.setAttribute('descricao', element.descricao)
+    card.setAttribute('preco', element.preco)
+    
+    container.appendChild(card)
+});
+
+const fetchBebida = async function () {
+    const url = ``
+    const response =  await fetch(url);
+    const data = await response.json();
+    return data
+}
+
+const bebida = await fetchBebida();
+console.log(bebida);
+
+bebida.forEach(element => {
+    const container = document.querySelector('.promocoes')
+    const card = document.createElement('.card_promo')
+
+    card.setAttribute('imagem', element.imagem)
+    card.setAttribute('nome', element.nome)
+    card.setAttribute('descricao', element.descricao)
+    card.setAttribute('preco', element.preco)
+    
+    container.appendChild(card)
+});
